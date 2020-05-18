9ee99ffa1492abaff12adf9f6bda423a26ad41d2
Alun Turner <alun.turner@googlemail.com>
2020-05-18 10:14:53 +0100

    material UI button and heading added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Typography variant="h1" gutterBottom="true">
+        PWA/Material UI test
+      </Typography>
+      <Button variant="contained" color="primary" size="large">
+        LIKE AND SUBSCRIBE
+      </Button>
+    </div>
+  );
 }
 
 export default App;

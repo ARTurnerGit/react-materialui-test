9ecc73bbb596074c3f6e52f65dd65c1043ee9bd5
Alun Turner <alun.turner@googlemail.com>
2020-05-18 11:38:20 +0100

    rebuild for netlify

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
       <Link href="/" underline="hover">
         GO BACK
       </Link>
-      <Typography variant="body1" gutterBottom={true} gutterTop={true}>
+      <Typography variant="body1" gutterBottom={true}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quia sed
         veritatis modi iste ex non officiis doloremque, magnam aut eligendi illo
         assumenda ut porro, pariatur voluptas velit! In, maxime.
